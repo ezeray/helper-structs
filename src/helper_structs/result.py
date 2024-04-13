4e5fd72505121f92cb72f08551403ea51d668e75
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2024-04-13 17:05:45 -0300

    Added new map method and bind method
    Overwrote the previous map method which was a different specification
    from the Rust Result map, so I made it more "compliant". At the same
    time, extracted the contents of map into a new method bind, which is the
    way that I had it structured a long way back. Also overrode the right
    shift operator to chain bind calls.
    
    Added docstring tests for more methods as well
    
    On branch develop
    Changes to be committed:
    	modified:   src/helper_structs/__pycache__/_types.cpython-312.pyc
    	modified:   src/helper_structs/__pycache__/result.cpython-312.pyc
    	modified:   src/helper_structs/result.py

--- src/helper_structs/result.py
+++ src/helper_structs/result.py
@@ -1,6 +1,6 @@
 import traceback
 from typing import Any, Generic, Callable
-from ._types import T, E, U, F
+from helper_structs._types import T, E, U, F
 
 
 class Result(Generic[T, E]):
@@ -234,12 +234,81 @@ class Result(Generic[T, E]):
             return self._val # type: ignore
         raise ValueError(msg)
 
-    def map(
+    def map(self, func: Callable[[T], U]) -> "Result[U, E] | Result[T, E]":
+        """
+        Feeds the contained value of an Ok variant into a callable,
+        basically mapping a Result<T, E> to a Result<U, E> (following
+        the rust docs). If it's an Err variant, leaves the container
+        unchanged.
+
+        >>> r = Result.Ok(10)
+        >>> r.map(lambda x: x ** 2)
+        Ok(100)
+
+        >>> e = Result.Err("fail")
+        >>> e.map(lambda x: x ** 2)
+        Err('fail')
+        """
+        return Result.Ok(func(self._val)) if self._is_ok else self # type: ignore
+
+    def map_err(self, func: Callable[[E], F]) -> "Result[T, E] | Result[T, F]":
+        """
+        Feeds the contained value of an Ok variant into a callable,
+        basically mapping a Result<T, E> to a Result<U, E> (following
+        the rust docs). If it's an Err variant, leaves the container
+        unchanged.
+
+        >>> r = Result.Ok(10)
+        >>> r.map_err(len)
+        Ok(10)
+
+        >>> e = Result.Err("fail")
+        >>> e.map_err(len)
+        Err(4)
+        """
+        return Result.Err(func(self._val)) if not self._is_ok else self # type:ignore
+
+    def bind(
             self,
             func: Callable[[T], U],
             *args: Any,
             **kwargs: Any,
         ) -> "Result[U, E] | Result[T, E]":
+        """
+        Feeds the contained value of an Ok variant into a callable,
+        basically mapping a Result<T, E> to a Result<U, E> (following
+        the rust docs). If it's an Err variant, leaves the container
+        unchanged. This mostly follows what map does, but adds to it.
+
+        This is the biggest point of difference with the Rust result
+        enum, since the bind in this instance allows passing arbitrary
+        args and kwargs, but also has an included try-catch block to
+        ensure that if there is any kind of failure the program doesn't
+        panic, instead returns an Err variant with a failure dictionary
+        that tracks the traceback, the callable, the Ok value, the
+        exception that was raised, and all args. These data can be
+        accessed from the self._failure dictionary
+
+        The idea here is to provide better coverage for dealing with
+        unexpected values passed and letting the program exit while 
+        fully logging any expection.
+
+        >>> sq = lambda x: x * x
+        >>> r = Result.Ok(10)
+        >>> r.bind(sq)
+        Ok(100)
+        >>> r = r.bind(sq).bind(sq)
+        >>> r
+        Ok(10000)
+        >>> gives_err = r.bind(len)
+        >>> gives_err
+        Err(TypeError("object of type 'int' has no len()"))
+        >>> gives_err._failure.get("func")
+        'len'
+        >>> e = Result.Err("fail")
+        >>> e.bind(sq)
+        Err('fail')
+        """
         if not self._is_ok:
             return self
         try:
@@ -265,9 +334,28 @@ class Result(Generic[T, E]):
             *args: Any,
             **kwargs: Any,
         ) -> "Result[U, E] | Result[T, E]":
-        return self.map(func, *args, **kwargs)
+        """
+        This is a wrapper over the bind method that overrides the right
+        shift operator to be able to chain bind calls using said
+        operator. I wanted to have something that was similar to the 
+        pipe operator, in particular for times when using the REPL.
 
-    def map_err(
+        >>> sq = lambda x: x * x
+        >>> r = Result.Ok(10)
+        >>> r >> (sq) >> (sq)
+        Ok(10000)
+        >>> new = r >> (sq) >> (len)
+        >>> new
+        Err(TypeError("object of type 'int' has no len()"))
+        >>> e = Result.Err("fail")
+        >>> e >> (sq) >> (sq)
+        Err('fail')
+        >>> e >> (len)
+        Err('fail')
+        """
+        return self.bind(func, *args, **kwargs)
+
+    def bind_err(
             self,
             func: Callable[[E], F],
             *args: Any,
