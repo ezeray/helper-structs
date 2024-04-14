9f5eacb08318b9ec9f7258368a6b08fa63a41439
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2024-04-13 22:07:00 -0300

    Created decorator for safe execution of functions
    To aid in general ease of use, I've addeda decorator that wraps a
    function in a try-catch block, ensuring that it returns a Result object,
    in particular making sure that the Err variant has the proper failure
    tracking.
    
    On branch feature/safe-execution-decorator
    Changes to be committed:
    	new file:   src/helper_structs/safe_exec.py

---
+++ src/helper_structs/safe_exec.py
@@ -0,0 +1,43 @@
+from functools import wraps
+from helper_structs import Ok, Err, Result
+from failure import FailureContainer
+
+
+def safe_exec(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Result:
+        try:
+            return Ok(func(*args, **kwargs))
+        except Exception as e:
+            f = FailureContainer(
+                func=func.__name__,
+                args=args,
+                kwargs=kwargs,
+                exception=e,
+            )
+            return Err(
+                    e,
+                    failure=f.as_dict()
+                )
+    return wrapper
+
+
+@safe_exec
+def add_one(a):
+    return a + 1
+
+
+def main():
+    print(add_one(2))
+    print(add_one("hello world"))
+    an_err = add_one("hello_world")
+    print(an_err.is_ok)
+    print(an_err._failure)
+
+    r = add_one(2)
+    print(r.is_ok)
+    print(r.unwrap_or(0))
+
+if __name__ == "__main__":
+    main()
+
